1e71965e44c6a8187ce208aedb3d5b7683dc8f98
gabrinovas <gabriel.novas@aimen.es>
2025-09-05 16:36:53 +0200

    Dockerfile Changes - avoiding third party dependencies

--- check.sh
+++ check.sh
@@ -1,60 +1,29 @@
 #!/bin/bash
 
-# Specify the container name and local image
+# Specify the container name
 CONTAINER_NAME="drims2"
-IMAGE_NAME="mi_drims2:local"
+IMAGE_NAME="gabrinovas/drims2:2025"
 
-echo "Using local image: $IMAGE_NAME..."
+# Pull the latest image
+echo "Pulling the latest image: $IMAGE_NAME..."
+docker pull $IMAGE_NAME
 
-# Grant X permissions (si es necesario)
-xhost +local:root
+# Check if the container exists
+if docker ps -a | grep -q $CONTAINER_NAME; then
+    echo "Container $CONTAINER_NAME exists."
 
-# Remove existing container if it exists
-if docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
-    echo "Container $CONTAINER_NAME exists. Removing it..."
-    docker rm -f $CONTAINER_NAME
+    # Check if the container is running
+    if [ "$(docker inspect -f {{.State.Running}} $CONTAINER_NAME)" == "true" ]; then
+        echo "Container $CONTAINER_NAME is running. Stopping it now..."
+        docker stop $CONTAINER_NAME
+        docker rm $CONTAINER_NAME
+    else
+        echo "Container $CONTAINER_NAME is not running."
+        docker rm $CONTAINER_NAME
+    fi
+else
+    echo "Container $CONTAINER_NAME does not exist."
 fi
 
-# Run container and execute the check script
-docker run -it \
-    --user drims \
-    -v /dev:/dev \
-    -v /dev/bus/usb:/dev/bus/usb \
-    --device=/dev/bus/usb \
-    --device-cgroup-rule='c 189:* rmw' \
-    -v /etc/udev/rules.d:/etc/udev/rules.d \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --net=host \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="$(pwd)/drims_ws:/home/drims/drims_ws" \
-    --volume="$(pwd)/bags:/bags" \
-    --name $CONTAINER_NAME \
-    -w /home/drims \
-    $IMAGE_NAME \
-    bash /home/drims/check_script.sh
-    # bash -c "bash /home/drims/check_script.sh; exec bash"
-    # bash -c "source /home/drims/drims_ws/install/setup.bash; bash /home/drims/check_script.sh; exec bash"
-    # bash -c "source /home/drims/drims_ws/install/setup.bash; ros2 run mi_drims2 check_script; exec bash"
-    # bash -c "source /home/drims/drims_ws/install/setup.bash; ros2 run mi_drims2 check_script; exec bash"
+docker run -it  --user drims -v /dev:/dev -v /dev/bus/usb:/dev/bus/usb --device=/dev/bus/usb --device-cgroup-rule='c 189:* rmw'  -v /etc/udev/rules.d:/etc/udev/rules.d --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --net=host --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="$(pwd)/drims_ws:/home/drims/drims_ws" --volume="$(pwd)/bags:/bags"  --name drims2 -w /home/drims $IMAGE_NAME bash /home/drims/check_script.sh
 
-# Copy .bash_aliases
-docker cp $(pwd)/.bash_aliases $CONTAINER_NAME:/home/drims/.bash_aliases    
-docker cp $(pwd)/check_script.sh $CONTAINER_NAME:/home/drims/check_script.sh
-docker exec -it $CONTAINER_NAME chmod +x /home/drims/check_script.sh
-docker exec -it $CONTAINER_NAME bash -c "source /home/drims/drims_ws/install/setup.bash; bash /home/drims/check_script.sh; exec bash"
-# docker exec -it $CONTAINER_NAME bash -c "source /home/drims/drims_ws/install/setup.bash; ros2 run mi_drims2 check_script; exec bash"
-# docker exec -it $CONTAINER_NAME bash -c "bash /home/drims/check_script.sh; exec bash"
-
-# Stop and remove the container after the script execution
-docker rm -f $CONTAINER_NAME
-echo "Container $CONTAINER_NAME has been removed."
-
-# Revoke X permissions (si se concedieron)
-xhost -local:root
-echo "X permissions revoked."
-
-echo "Check script execution completed."
-
-# Note: If you want to keep the container running after executing the check script,
-# you can remove the 'docker rm -f $CONTAINER_NAME' line above.
--- start.sh
+++ start.sh
@@ -1,51 +1,35 @@
 #!/bin/bash
 
-# Specify the container name and local image
+# Specify the container name
 CONTAINER_NAME="drims2"
-IMAGE_NAME="mi_drims2:local"
+IMAGE_NAME="gabrinovas/drims2:2025"
+#IMAGE_NAME="my_image:local"
 
-echo "Using local image: $IMAGE_NAME..."
+# Pull the latest image
+echo "Pulling the latest image: $IMAGE_NAME..."
+docker pull $IMAGE_NAME
 
 # Grant X permissions
+#xhost +si:localuser:$(whoami)
 xhost +local:root
+# Check if the container exists
+if docker ps -a | grep -q $CONTAINER_NAME; then
+    echo "Container $CONTAINER_NAME exists."
 
-# Remove existing container if it exists
-if docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
-    echo "Container $CONTAINER_NAME exists. Removing it..."
-    docker rm -f $CONTAINER_NAME
+    # Check if the container is running
+    if [ "$(docker inspect -f {{.State.Running}} $CONTAINER_NAME)" == "true" ]; then
+        echo "Container $CONTAINER_NAME is running. Stopping it now..."
+        docker stop $CONTAINER_NAME
+        docker rm $CONTAINER_NAME
+    else
+        echo "Container $CONTAINER_NAME is not running."
+        docker rm $CONTAINER_NAME
+    fi
+else
+    echo "Container $CONTAINER_NAME does not exist."
 fi
 
-# Run container
-docker run -dit \
-    --user drims \
-    --privileged \
-    -v /dev:/dev \
-    -v /dev/bus/usb:/dev/bus/usb \
-    --device=/dev/bus/usb \
-    --device-cgroup-rule='c 189:* rmw' \
-    -v /etc/udev/rules.d:/etc/udev/rules.d \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --net=host \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="$(pwd)/drims_ws:/home/drims/drims_ws" \
-    --volume="$(pwd)/bags:/home/drims/bags" \
-    --name $CONTAINER_NAME \
-    -w /home/drims \
-    $IMAGE_NAME
+docker run -dit  --user drims --privileged -v /dev:/dev -v /dev/bus/usb:/dev/bus/usb --device=/dev/bus/usb --device-cgroup-rule='c 189:* rmw'  -v /etc/udev/rules.d:/etc/udev/rules.d --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --net=host --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="$(pwd)/drims_ws:/home/drims/drims_ws" --volume="$(pwd)/bags:/home/drims/bags"  --name drims2 -w /home/drims $IMAGE_NAME
 
-# Copy .bash_aliases
 docker cp $(pwd)/.bash_aliases $CONTAINER_NAME:/home/drims/.bash_aliases
-
-# Open bash
 docker exec -it $CONTAINER_NAME bash
-
-# Revoke X permissions
-xhost -local:root
-echo "X permissions revoked."
-
-echo "Container $CONTAINER_NAME is running. You can now execute commands inside the container."
-echo "To stop and remove the container, use: docker rm -f $CONTAINER_NAME"
-
-# Note: If you want to keep the container running after exiting bash,
-# you can remove the 'docker rm -f $CONTAINER_NAME' line above.
